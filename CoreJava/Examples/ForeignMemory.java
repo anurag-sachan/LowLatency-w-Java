b93bef11101f8af3dd950a3585ce99ee3e6a5c48
Anurag Sachan <connect2anurags@gmail.com>
2025-07-24 02:01:43 +0530

    Pending: ForeignMemory

---
+++ CoreJava/Examples/ForeignMemory.java
@@ -0,0 +1,21 @@
+package Examples;
+import jdk.incubator.foreign.*;
+
+// Foreign Memory Access API (Preview)
+// Requires: --enable-preview --add-modules jdk.incubator.foreign
+// Java22+
+public class ForeignMemory {
+    public static void main(String[] args) {
+        // Allocate native memory using Arena (auto-closeable scope)
+        try (Arena arena = Arena.ofConfined()) {
+            MemorySegment segment = arena.allocate(ValueLayout.JAVA_INT); // 4 bytes
+
+            // Write to memory
+            segment.set(ValueLayout.JAVA_INT, 0, 123);
+
+            // Read from memory
+            int value = segment.get(ValueLayout.JAVA_INT, 0);
+            System.out.println("Value from native memory: " + value);
+        }
+    }
+}
