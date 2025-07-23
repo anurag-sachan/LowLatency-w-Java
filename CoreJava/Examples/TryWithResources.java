944d92f7be5dcddb6350068ed8a07ffbd141f124
Anurag Sachan <connect2anurags@gmail.com>
2025-07-23 16:30:04 +0530

    IOFile

--- CoreJava/Examples/TryWithResources.java
+++ CoreJava/Examples/TryWithResources.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 
 public class TryWithResources {
     public static void main(String[] args) throws Exception {
-        try (BufferedReader br = new BufferedReader(FileReader("data.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("/Users/anurag/Data/scrap/javaLowLatency/CoreJava/Examples/source.txt"))) {
             System.out.println(br.readLine());
         } catch (Exception e){
             System.out.println(e);
