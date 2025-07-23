b0790caa576a5b273e93ea73fa0e2ed22702ed2a
Anurag Sachan <connect2anurags@gmail.com>
2025-07-23 18:06:18 +0530

    ReflectAPI: access private fxn/methods

--- CoreJava/Examples/Reflect.java
+++ CoreJava/Examples/Reflect.java
@@ -4,18 +4,28 @@ package Examples;
 class Secret {
     String name = "anurag";
     private String hidden = "secret";
+
+    private String hiddenFxn(){
+        return "secret from function";
+    }
 }
 
 class Reflect {
     public static void main(String[] args) throws Exception {
+        var s = new Secret();
         // System.out.println(s.name);
         // System.out.println(s.hidden);
+        // System.out.println(s.hiddenFxn());
         
-        var s = new Secret();
         // getField() only works for public fields
         var field = Secret.class.getDeclaredField("hidden");
+        var functionValue = Secret.class.getDeclaredMethod("hiddenFxn");
+
         // using reflection, bypasses Java's access control checks
         field.setAccessible(true);
+        functionValue.setAccessible(true);
+
         System.out.println(field.get(s));
+        System.out.println(functionValue.invoke(s));
     }
 }
